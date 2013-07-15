49c9e7756d4501660b0adffc469478aff8711cfa
Robbe Van der Gucht <robbe.vandergucht@gmail.com>
2013-07-16 01:01:56 +0200

    html and css template files added

---
+++ home.pp
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>{TITLE}</title>
+    <link rel="stylesheet" type="text/css" href="{CSS}" />
+</head>
+
+<body>
+
+<div id=container>
+    <h1>{TITLE}</h1>
+    <p>Here are the files!</p>
+    <hr />
+    <!-- list files -->
+    {CONTENT}
+    <hr />
+</div>
+
+
+</body>
+</html>
