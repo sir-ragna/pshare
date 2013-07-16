d758181765c74d168a844a2bf56709372e76d980
Robbe Van der Gucht <robbe.vandergucht@gmail.com>
2013-07-16 19:10:26 +0200

    pshare.py is going to be a reworked version of share.py

--- pshare.py
+++ pshare.py
@@ -2,4 +2,33 @@
 #
 # Attempt to implement own version of a Request handler.
 # Rewrite of share.py
+# docs:
+# http://docs.python.org/2/library/basehttpserver.html
 
+import SocketServer
+import BaseHTTPServer
+import os
+import cgi
+import urllib
+import sys
+import posixpath
+import shutil
+
+class PShare(BaseHTTPServer.BaseHTTPRequestHandler):
+    """Webserver that serves files from a dedicated folder."""
+
+    def __init__(self, *args):
+        # my own code that creates
+        # an object that contains settings
+        super().__init__(args)
+
+    def do_GET(self):
+        pass
+
+    def do_HEAD(self):
+        pass
+
+    def send_file(self, f):
+        """Send file(html or data) towards the requesting client"""
+        shutil.copyfileobj(f, self.wfile)
+        
